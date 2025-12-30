cacf802d9b7553df4148c4828716c10b62c833f7
BROCODES2024 <chetanreddyk394@gmail.com>
2025-12-30 12:47:14 +0530

    changes

---
+++ a_getting_started/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+// main function is the first code that runs in an executable rust program
+println!("Hello, world!");
+// In Rust, anything with `!` is a macro
+// println! is a macro
+// println does NOT exist
+// but if we are still making a call to println then its a function
+
+// how do we compile
+//first cd the path of main.rs
+//then do rustc main.rs and then do .\main.rs to get output
+//Cargo is Rust’s build system and package manager.
+}
