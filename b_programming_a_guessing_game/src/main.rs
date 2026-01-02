ef03e2a545132c7c26ecebec0f4f3bb0ea8940ac
BROCODES2024 <chetanreddyk394@gmail.com>
2026-01-02 10:55:37 +0530

    changes

--- b_programming_a_guessing_game/src/main.rs
+++ b_programming_a_guessing_game/src/main.rs
@@ -34,16 +34,19 @@ fn main() {
         }
     }
 }
-    //cmp is a method which compares two values
-    //match is similar to switch in c++
-    //why match insted of if else , cause match forces us to handle all cases
-    // pattern =>(arm) code ie if this pattern matches run the "code"
-    //here ordering is an enum it has three variants less,greaterm equal
-    // ordering is enum just like result is an enum
-    // since enums represent finite known cases so using match is best option
-    // if using an enum we must handle all variants
-    //we see that guess is already prensent but we again did let guess
-    // helpfully Rust allows us to shadow the previous value of guess with a new one. 
-    //Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables
-    //trim removes whitespaces and parse converts string to another type
-    //loop keyword creates an infinte loop
+   
+    /*
+        cmp is a method which compares two values
+        match is similar to switch in c++
+        why match insted of if else , cause match forces us to handle all cases
+        pattern =>(arm) code ie if this pattern matches run the "code"
+        here ordering is an enum it has three variants less,greaterm equal
+        ordering is enum just like result is an enum
+        since enums represent finite known cases so using match is best option
+        if using an enum we must handle all variants
+        we see that guess is already prensent but we again did let guess
+        helpfully Rust allows us to shadow the previous value of guess with a new one. 
+        Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables
+        trim removes whitespaces and parse converts string to another type
+        loop keyword creates an infinte loop
+     */
